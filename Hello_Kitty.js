55ee2987e22435e30ae1bf5b17db3ecd27dc0ab4
abdelrahman nashaat <abdulrahman.nashaat@gmail.com>
2023-07-18 20:01:26 +0300

    hello kitty image downloader
    creating a hello kitty website that allows users to get a hello kitty image with a background image and download it

---
+++ Hello_Kitty.js
@@ -0,0 +1,24 @@
+const images = [
+"http://drive.google.com/uc?export=view&id=1Do9YDl86AetVcQtgyK5i_dfsbQiIu6L0",
+"http://drive.google.com/uc?export=view&id=1wulbYuBD7-Dy5Ac9158NTNDtunXmiuJn",
+"http://drive.google.com/uc?export=view&id=1TzcM3fSu0_Xwh_rHsLlqutjcsdOfIiD9",
+"http://drive.google.com/uc?export=view&id=1-H9ngRa7FhIW15rkzKawILP-f8eYYAKh"
+];
+var currentIndex = 0;
+
+function showImage() {
+    var currentImage = document.querySelector(".container-image img");
+    currentImage.src = images[currentIndex];
+}
+
+document.getElementById("btn").addEventListener("click", function() {
+    currentIndex++;
+    if(currentIndex >= images.length){
+        currentIndex = 0;
+    }
+    showImage();
+})
+
+// https://drive.google.com/file/d/1wulbYuBD7-Dy5Ac9158NTNDtunXmiuJn/view?usp=sharing
+// https://drive.google.com/file/d/1TzcM3fSu0_Xwh_rHsLlqutjcsdOfIiD9/view?usp=sharing
+// https://drive.google.com/file/d/1-H9ngRa7FhIW15rkzKawILP-f8eYYAKh/view?usp=sharing
