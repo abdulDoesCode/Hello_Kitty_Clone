7b3c6d4b2ae5e7af2d2d1cc02366f36f131181c4
abdelrahman nashaat <abdulrahman.nashaat@gmail.com>
2023-07-20 13:24:07 +0300

    hello kitty text area addition
    adding a randomized quote generator for hello kitty

--- Hello_Kitty.js
+++ Hello_Kitty.js
@@ -1,20 +1,30 @@
+// switching between images
 const images = [
 "http://drive.google.com/uc?export=view&id=1Do9YDl86AetVcQtgyK5i_dfsbQiIu6L0",
 "http://drive.google.com/uc?export=view&id=1wulbYuBD7-Dy5Ac9158NTNDtunXmiuJn",
 "http://drive.google.com/uc?export=view&id=1TzcM3fSu0_Xwh_rHsLlqutjcsdOfIiD9",
 "http://drive.google.com/uc?export=view&id=1-H9ngRa7FhIW15rkzKawILP-f8eYYAKh"
 ];
-var currentIndex = 0;
+let currentIndex = 0;
+let  currentText = document.getElementById("text-area");
 
 function showImage() {
-    var currentImage = document.querySelector(".container-image img");
+    let currentImage = document.querySelector(".container-image img");
     currentImage.src = images[currentIndex];
-}
 
-document.getElementById("btn").addEventListener("click", function() {
     currentIndex++;
     if(currentIndex >= images.length){
         currentIndex = 0;
     }
+}
+
+function changeText(){
+    currentText.val = " ";
+    
+}
+
+document.getElementById("btn").addEventListener("click", function() {
     showImage();
+    changeText();
 })
+
