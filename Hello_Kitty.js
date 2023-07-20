1910f931288f3a17cd4ba7bd50d4246dc61d7fd7
abdelrahman nashaat <abdulrahman.nashaat@gmail.com>
2023-07-20 14:24:19 +0300

    text box with hello kitty quotes is now working

--- Hello_Kitty.js
+++ Hello_Kitty.js
@@ -5,6 +5,15 @@ const images = [
 "http://drive.google.com/uc?export=view&id=1TzcM3fSu0_Xwh_rHsLlqutjcsdOfIiD9",
 "http://drive.google.com/uc?export=view&id=1-H9ngRa7FhIW15rkzKawILP-f8eYYAKh"
 ];
+
+const quotes = [
+"You can never have too many friends",
+"Friendship is the purr-fect gift that warms the heart and makes every day meow-gical.",
+"With a smile and a bow, I'll sprinkle kindness everywhere I go, bringing happiness to all!",
+"Love and kindness bloom like flowers in a Hello Kitty world, spreading joy to everyone!",
+"Embrace your uniqueness like I embrace my bow; it's what makes you special and shines from within!",
+"Let's share hugs, laughter, and dreams, making the world a cuddly and cheerful place!"
+]
 let currentIndex = 0;
 let  currentText = document.getElementById("text-area");
 
@@ -18,9 +27,8 @@ function showImage() {
     }
 }
 
-function changeText(){
-    currentText.val = " ";
-    
+const changeText = () => {
+    currentText.value = quotes[Math.floor(Math.random() * quotes.length)];
 }
 
 document.getElementById("btn").addEventListener("click", function() {
